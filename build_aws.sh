faae4853ac9175a4b141e637d610ac172fec40d0
Rajdeep <rajdeep.biswas@laerdal.com>
2026-02-08 04:53:09 +0000

    Add build script for AWS deployment

---
+++ build_aws.sh
@@ -0,0 +1,17 @@
+PROJECT_ROOT=$(pwd)
+BACKEND_DIR="$PROJECT_ROOT/backend"
+FRONTEND_DIR="$PROJECT_ROOT/frontend"
+
+cd $PROJECT_ROOT
+git pull origin master
+cd $BACKEND_DIR
+npm install
+pm2 status
+pm2 restart all
+cd $FRONTEND_DIR
+npm install
+npm run build
+sudo rm -rf /usr/share/nginx/html/*
+sudo cp -r dist/ro-service-frontend/browser/* /usr/share/nginx/html/
+sudo systemctl reload nginx
+
